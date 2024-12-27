04e9359793557856ab4753ed31d69d4f9854670c
rojanagunoori <nagunooriroja@gmail.com>
2024-12-27 18:56:45 +0530

    change

--- context/FiltersContext.tsx
+++ context/FiltersContext.tsx
@@ -28,7 +28,66 @@ export const FiltersProvider = ({ children }: { children: React.ReactNode })=> {
    const [activeFilterOptions, setActiveFilterOptions] = useState(null);
 
  
-  const updateOptions = (collection) => {
+  /*const updateOptions = (collection) => {
+    let newOptions;
+    switch (collection) {
+      case 'Apple Watch Series 10':
+        newOptions = {
+          size: ['42mm', '46mm'],
+          case: ['Aluminum', 'Titanium'],
+          band: [
+            'Stainless Steel',
+            'Sport Loop',
+            'Sport Band',
+            'FineWoven',
+            'Braided Solo Loop',
+            'Solo Loop',
+            'Nike Sport Loop',
+            'Nike Sport Band',
+          ],
+        };
+        break;
+      case 'Apple Watch Hermès Series 10':
+        newOptions = {
+          size: ['42mm', '46mm'],
+          case: ['Titanium'],
+          band: ['Hermès Toile H', 'Hermès Torsade', 'Hermès Kilim', 'Hermès Grand H'],
+        };
+        break;
+      case 'Apple Watch SE':
+        newOptions = {
+          size: ['40mm', '44mm'],
+          case: ['Aluminum'],
+          band: [
+            'Stainless Steel',
+            'Sport Loop',
+            'Sport Band',
+            'FineWoven',
+            'Braided Solo Loop',
+            'Solo Loop',
+            'Nike Sport Loop',
+            'Nike Sport Band',
+          ],
+        };
+        break;
+      default:
+        newOptions = {
+          size: [],
+          case: [],
+          band: [],
+        };
+    }
+
+    setOptions(newOptions);
+    setSelectedOptions({
+      size: newOptions.size[0] || null,
+      case: newOptions.case[0] || null,
+      band: newOptions.band[0] || null,
+    });
+    setSelectedCollection(collection);
+  };*/
+
+  const updateOptions = (collection: string) => {
     let newOptions;
     switch (collection) {
       case 'Apple Watch Series 10':
@@ -86,6 +145,7 @@ export const FiltersProvider = ({ children }: { children: React.ReactNode })=> {
     });
     setSelectedCollection(collection);
   };
+
 
 
   const handleFilterClick = (filter) => {
